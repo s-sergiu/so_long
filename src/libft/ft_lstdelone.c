26b196f1230c7b7d4c3ae315353275a338b8f260
Sergiu Ster <ssergiu@student.42heilbronn.de>
2022-11-25 23:52:39 +0100

    cleanup

---
+++ src/libft/ft_lstdelone.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssergiu <ssergiu@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 00:16:16 by ssergiu           #+#    #+#             */
+/*   Updated: 2022/11/25 08:51:32 by ssergiu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	del(lst->position);
+	lst->next = NULL;
+	free(lst);
+}
